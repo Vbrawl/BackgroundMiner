99dea8b421cd104bd20ae28e40aa357d8b167d12
Vbrawl <greninjagr@gmail.com>
2023-04-20 11:30:32 +0300

    Modified to use the TempManager.

--- lib/Downloader.py
+++ lib/Downloader.py
@@ -1,7 +1,6 @@
 import requests
 import platform
-import tempfile
-import os.path
+from lib.TempManager import TempManager
 
 
 class Downloader:
@@ -21,16 +20,18 @@ class Downloader:
         }
 
         self.NAMESPACE = f"/{NAMESPACE_MAP[platform.system().upper()]}{platform.architecture()[0][:2]}/"
-        self.TEMPDIR = tempfile.mkdtemp("mnr", "bg")
     
     def _download(self, file:str) -> str:
+        filepath = TempManager.get_path(file)
+
         r = requests.get(self.URL + self.NAMESPACE + file, stream=True, timeout=30, verify=False)
-        filepath = os.path.join(self.TEMPDIR, file)
-        with open(filepath, 'wb') as f:
-            for chunk in r.iter_content(self.CHUNK_SIZE):
-                if chunk:
-                    f.write(chunk)
+        f = TempManager.open_file(filepath, 'wb')
+
+        for chunk in r.iter_content(self.CHUNK_SIZE):
+            if chunk:
+                f.write(chunk)
         
+        f.close()
         return filepath
 
 
@@ -38,4 +39,3 @@ class Downloader:
 if __name__ == "__main__":
     dd = Downloader()
     dd._download(dd.MINER_PATH)
-    print(dd.TEMPDIR)
