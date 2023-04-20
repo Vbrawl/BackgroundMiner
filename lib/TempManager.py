85f726c0f09847b3df79f950b9792c342b353795
Vbrawl <greninjagr@gmail.com>
2023-04-20 11:30:16 +0300

    Created a manager to manager the temp directory.

---
+++ lib/TempManager.py
@@ -0,0 +1,15 @@
+import tempfile, os
+from typing import IO
+
+
+
+class TempManager:
+    TEMPDIR = tempfile.mkdtemp("mnr", "bg")
+
+    @staticmethod
+    def get_path(filename:str) -> str:
+        return os.path.join(TempManager.TEMPDIR, filename)
+
+    @staticmethod
+    def open_file(filename:str, mode = 'r') -> IO:
+        return open(filename, mode)
