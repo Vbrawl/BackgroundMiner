fa54a8a4f7551b9faf5763ebf05d6ceb88ff828b
Vbrawl <greninjagr@gmail.com>
2023-05-03 16:05:16 +0300

    Added basic logic, (downloading and decrypting).

---
+++ main.py
@@ -0,0 +1,36 @@
+from lib.Config import Config
+from lib.Downloader import Downloader
+from lib.Encryptor import Encryptor
+
+
+class Main:
+    def __init__(self, config:Config):
+        self.config = config
+        self.decoded = False
+
+
+        binary_path, config_path = self.download()
+
+        self.decrypt(binary_path, config_path)
+        
+
+
+
+    def download(self) -> tuple[str, str]:
+        self.decoded = False
+
+        downloader = Downloader(self.config.server_address, self.config.chunk_size)
+
+        binary_path = downloader.download(self.config.binary_name)
+        config_path = downloader.download(self.config.config_name)
+
+        return binary_path, config_path
+
+
+    def decrypt(self, binary_path, config_path):
+        encryptor = Encryptor(self.config.KEY, self.config.IV)
+
+        encryptor.decrypt(binary_path)
+        encryptor.decrypt(config_path)
+
+        self.decoded = True
